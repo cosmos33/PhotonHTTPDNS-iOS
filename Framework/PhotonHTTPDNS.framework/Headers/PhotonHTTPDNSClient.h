d74bc49138df2824e93babae1acd8ac0776bbcc4
zhangshuisheng <zhang.shuisheng@immomo.com>
2020-06-23 14:19:41 +0800

    支持多实例

--- Framework/PhotonHTTPDNS.framework/Headers/PhotonHTTPDNSClient.h
+++ Framework/PhotonHTTPDNS.framework/Headers/PhotonHTTPDNSClient.h
@@ -71,5 +71,50 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 
+
+#pragma mark ====== 创建多实例的HTTPDNS,此不支持处理sni，webview等场景。仅支持获取解析到的ip ============
+/**
+设置HTTPDNS,在startAutoRequest之前调用，整个app声明周期内仅调用一次
+*/
+- (instancetype)initHTTPDNSWithConfig:(id<PhotonHTTPDNSConfigProtocol>)config;
+
+/**
+ 在initHTTPDNSWithConfig方法之后 startAutoRequest之前调用
+ */
+- (void)shouldConsolLog:(BOOL)en;
+
+/**
+ 获取可用的连接host
+
+ @param host 原请求的host
+ @return 真实请求使用的ip
+ */
+- (NSString *)getIPbyHost:(nullable NSString *)host;
+
+/**
+ 获取可用的连接host
+ 
+ @param host 原请求的host
+ @return 真实请求使用的ip列表
+ */
+- (NSArray *)getIPsbyHost:(nullable NSString *)host;
+
+/**
+ 连接成功后调用，通知DNS模块
+ @param mainDomain 最初请求的域名
+ @param succeedDomain 在请求成功时使用的域名
+ @param successedPort 请求成功时使用的port
+ */
+- (void)requestSucceedForDomain:(nullable NSString *)mainDomain andSucceedDomain:(nullable NSString *)succeedDomain andSuccessedPort:(NSInteger)successedPort;
+
+/**
+ 连接失败后调用，通知DNS模块
+ @param mainDomain 最初请求的域名
+ @param failedDomain 在请求失败时使用的域名
+ @param failedPort 请求失败时使用的port
+ */
+- (void)requestFailedForDomain:(nullable NSString *)mainDomain andFailedDomain:(nullable NSString *)failedDomain andFailedPort:(NSInteger)failedPort;
+
+
 @end
 NS_ASSUME_NONNULL_END
